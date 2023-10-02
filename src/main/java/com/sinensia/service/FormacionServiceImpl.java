704c73b9746db45d5ef21fe5090f6c2e91ef4102
sreturns <moneytrees@gmail.com>
2023-10-02 23:13:52 +0200

    improved validations

--- src/main/java/com/sinensia/service/FormacionServiceImpl.java
+++ src/main/java/com/sinensia/service/FormacionServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import io.micrometer.common.util.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -22,82 +23,95 @@ import com.sinensia.model.Formacion;
 @Service
 public class FormacionServiceImpl implements FormacionService {
 
-	/**
-	 * Inyectamos el Restamplate configurado en la clase de configuracion
-	 * Application.java
-	 */
-	@Autowired
-	RestTemplate template;
+    /**
+     * Inyectamos el Restamplate configurado en la clase de configuracion
+     * Application.java
+     */
+    @Autowired
+    RestTemplate template;
 
-	private final String URL_MICROSERVICIO_CURSOS = "http://localhost:8080/curso";
+    private final String URL_MICROSERVICIO_CURSOS = "http://localhost:8080/curso";
 
-	/**
-	 * Devolvemos la lista en formato del objeto Formacio, mapeandolo a traves de
-	 * CursoDto
-	 * 
-	 * @return List<Formacio>
-	 */
-	@Override
-	public List<Formacion> getAll() {
-		List<CursoDTO> list = Arrays.asList(template.getForObject(URL_MICROSERVICIO_CURSOS, CursoDTO[].class));
+    /**
+     * Devolvemos la lista en formato del objeto Formacio, mapeandolo a traves de
+     * CursoDto
+     *
+     * @return List<Formacio>
+     */
+    @Override
+    public List<Formacion> getAll() {
+        List<CursoDTO> list = Arrays.asList(template.getForObject(URL_MICROSERVICIO_CURSOS, CursoDTO[].class));
 
-		// Mapeamos
-		List<Formacion> listaFormacion = list.stream().map(cursoDTO -> {
-			Formacion formacion = new Formacion();
-			formacion.setCurso(cursoDTO.getNombre());
-			formacion.setAsignaturas(calculate(cursoDTO.getDuracion()));
-			formacion.setPrecio(cursoDTO.getPrecio());
-			return formacion;
-		}).collect(Collectors.toList());
+        // Mapeamos
+        List<Formacion> listaFormacion = list.stream().map(cursoDTO -> {
+            Formacion formacion = new Formacion();
+            formacion.setCurso(cursoDTO.getNombre());
+            formacion.setAsignaturas(calculate(cursoDTO.getDuracion()));
+            formacion.setPrecio(cursoDTO.getPrecio());
+            return formacion;
+        }).collect(Collectors.toList());
 
-		return listaFormacion;
-	}
+        return listaFormacion;
+    }
 
-	/**
-	 * Guardamos el recurso formacion mapeandolo a traves del CursoDto
-	 * 
-	 * @param formacion
-	 * @return List<Formacion>
-	 */
-	@Override
-	public List<Formacion> save(Formacion formacion) {
-		int duracion = calculateDuration(formacion.getAsignaturas());
+    /**
+     * Guardamos el recurso formacion mapeandolo a traves del CursoDto
+     *
+     * @param formacion
+     * @return List<Formacion>
+     */
+    @Override
+    public List<Formacion> save(Formacion formacion) {
+        if (!isValidFormacion(formacion)) {
+            throw new IllegalArgumentException("No es valida la formacion que has introducido");
+        }
+        int duracion = calculateDuration(formacion.getAsignaturas());
 
-		CursoDTO curso = new CursoDTO(formacion.getCurso(), duracion, formacion.getPrecio());
+        CursoDTO curso = new CursoDTO(formacion.getCurso(), duracion, formacion.getPrecio());
 
-		template.postForLocation(URL_MICROSERVICIO_CURSOS, curso);
+        template.postForLocation(URL_MICROSERVICIO_CURSOS, curso);
 
-		List<CursoDTO> list = Arrays.asList(template.getForObject(URL_MICROSERVICIO_CURSOS, CursoDTO[].class));
+        return getAll();
+    }
 
-		List<Formacion> listaFormacion2 = list.stream().map(cursoDTO -> {
-			Formacion formacion2 = new Formacion();
-			formacion2.setCurso(cursoDTO.getNombre());
-			formacion2.setAsignaturas(calculate(cursoDTO.getDuracion()));
-			formacion2.setPrecio(cursoDTO.getPrecio());
-			return formacion2;
-		}).collect(Collectors.toList());
-		return listaFormacion2;
+    /**
+     * Metodo para calcular el numero de asignaturas
+     *
+     * @param duracion
+     * @return int
+     */
+    private int calculate(int duracion) {
+        return (duracion >= 50) ? 10 : 5;
+    }
 
-	}
+    /**
+     * Metodo para calcular la duracion del curso
+     *
+     * @param asignaturas
+     * @return asiganturas
+     */
+    private int calculateDuration(int asignaturas) {
+        return asignaturas * 10;
+    }
 
-	/**
-	 * Metodo para calcular el numero de asignaturas
-	 * 
-	 * @param duracion
-	 * @return int
-	 */
-	private int calculate(int duracion) {
-		return (duracion >= 50) ? 10 : 5;
-	}
+    private boolean isValidFormacion(Formacion formacion) {
+        if (formacion == null) {
+            return false;
+        }
 
-	/**
-	 * Metodo para calcular la duracion del curso
-	 * 
-	 * @param asignaturas
-	 * @return asiganturas
-	 */
-	private int calculateDuration(int asignaturas) {
-		return asignaturas * 10;
-	}
+        if (StringUtils.isBlank(formacion.getCurso())) {
+            return false;
+        }
+
+        if (formacion.getAsignaturas() <= 0) {
+            return false;
+        }
+
+        if (formacion.getPrecio() <= 0) {
+            return false;
+        }
+
+        return true;
+    }
 
 }
